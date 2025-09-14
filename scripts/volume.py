8db94b1be3d1e5e3b382ead795934b2e2e29fa59
unknown <parnikab127@gmail.com>
2025-09-14 20:52:31 +0530

    finished the colume functionality, have to add better ui

--- scripts/volume.py
+++ scripts/volume.py
@@ -4,6 +4,23 @@ import time
 import handtracking as htm
 import math
 
+#pycaw for volume control
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from comtypes import CLSCTX_ALL
+device = AudioUtilities.GetSpeakers()
+interface = device.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = interface.QueryInterface(IAudioEndpointVolume)
+'''print(f"Audio output: {device.FriendlyName}")
+print(f"- Volume level: {volume.GetMasterVolumeLevel()} dB")
+print(f"- Volume range: {volume.GetVolumeRange()[0]} dB - {volume.GetVolumeRange()[1]} dB")
+volume.SetMasterVolumeLevel(-20.0, None)'''
+
+
+volRange=volume.GetVolumeRange()
+volume.GetMasterVolumeLevel()
+minVol=volRange[0]
+maxVol=volRange[1]
+
 wcam,hcam=640,480
 
 cap=cv2.VideoCapture(0)
@@ -11,7 +28,8 @@ cap.set(3,wcam) #propid 3 is width
 cap.set(4,hcam) #propid 4 is width
 
 detector=htm.handDetector(mindetconfidence=0.7)
-
+converted_vol=0
+volbar=400
 while True:
     success,img=cap.read()#VideoCapture to start capturing video
     img=detector.findHands(img)
@@ -31,12 +49,26 @@ while True:
         cv2.line(img,(x1,y1),(x2,y2),(255,255,255),3)#draw line between the two points
 
         length=math.hypot(x2-x1,y2-y1)#the hypotenuse of the two points to have a relation with the distance and using it to make it relatable to the volume  
-        print(length)
+        #print(length)
+
+        #hand range: 20 to 300
+        #volume range: -65 to 0
+        converted_vol=np.interp(length,[20,300],[minVol,maxVol])
+        volbar=np.interp(length,[20,300],[400,150])
+        volperc=np.interp(length,[20,300],[0,100])
+        #print(converted_vol)
+        volume.SetMasterVolumeLevel(converted_vol,None)
+        converted_vol=0
 
         if length<50:
             cv2.circle(img,(cx,cy),10,(0,255,0),cv2.FILLED)
         elif length>200:
             cv2.circle(img,(cx,cy),10,(255,0,0),cv2.FILLED)
+
+        cv2.rectangle(img,(50,150),(85,400),(0,255,0),2)#volume bar
+        cv2.rectangle(img,(50,int(volbar)),(85,400),(0,255,0),cv2.FILLED)#volume bar
+        cv2.putText(img, f'{int(volperc)} %', (40, 430), cv2.FONT_HERSHEY_COMPLEX,
+                1, (0, 255, 0), 2)
 
     cv2.imshow("Image",img) #show the image on screen
     if cv2.waitKey(1) & 0xFF==ord('q') or cv2.getWindowProperty("Image", cv2.WND_PROP_VISIBLE) < 1:
